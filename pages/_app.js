0102a10c2ee235394412200c3044b6eb4ed27a1e
Asep Dera Purnama <asep.derrapurnama@gmail.com>
2022-05-13 19:46:32 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'main'
+      }
+    }
+  },
+  colors: {
+    'main': '#232224',//'#121122',//
+    'secondary': '#EEECC9',//'#020623',//'#474448',//'#2d232e',
+    'accent': '#3d233e'//'#e0ddcf'
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
 }
 
 export default MyApp
